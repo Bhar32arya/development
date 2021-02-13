a5bf79bad6801e4019fe1676350d4570c00367fd
Bhar32arya <bhar32arya@gmail.com>
2021-02-13 20:50:04 +0530

    mockito test case practise

---
+++ Mockito/mockito/src/main/java/com/practise/mockito/service/DataBaseWriteException.java
@@ -0,0 +1,15 @@
+package com.practise.mockito.service;
+
+public class DataBaseWriteException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7426273141706709824L;
+
+	public DataBaseWriteException(String msg) {
+		// TODO Auto-generated constructor stub
+		
+		super(msg);
+	}
+}
---
+++ Mockito/mockito/src/main/java/com/practise/mockito/service/DataNotFoundException.java
@@ -0,0 +1,14 @@
+package com.practise.mockito.service;
+
+public class DataNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7006509769877070558L;
+
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+
+}
